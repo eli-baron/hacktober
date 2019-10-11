69b0b981fbcf180a8b6d5fdfecae79d0fc8e06eb
Juan Pablo Flores <jp@juanpabloflores.me>
2019-10-11 17:24:44 -0500

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,16 @@
+def suma(x,y):
+  """Esta funcion hace una suma"""
+
+def resta(x,y):
+  """Esta funcion hace una resta"""
+
+def division(x,y):
+  """Esta funcion hace una division"""
+
+def multiplicacion(x,y):
+  """Esta funcion hace una multiplicacion"""
+  
+def modulo(x,y):
+  """Esta funcion hace una suma"""
+
+
